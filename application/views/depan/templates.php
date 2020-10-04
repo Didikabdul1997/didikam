b14d7da43c13d6fd8fba1caaac82fd2da526a9ad
Lenovo <>
2020-10-04 13:47:05 +0700

    Update templates.php

--- application/views/depan/templates.php
+++ application/views/depan/templates.php
@@ -8,16 +8,6 @@
     <title>Welcome To Didikam</title>
     <!-- MDB icon -->
     <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
-    <!-- Google Fonts Roboto -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="<?= base_url(); ?>material/css/bootstrap.min.css">
-    <!-- Material Design Bootstrap -->
-    <link rel="stylesheet" href="<?= base_url(); ?>material/css/mdb.min.css">
-    <!-- Your custom styles (optional) -->
-    <link rel="stylesheet" href="<?= base_url(); ?>material/css/style.css">
 </head>
 <style type="text/css">
     html,
@@ -53,7 +43,7 @@
 </style>
 </head>
 
-<body>
+<body id="body">
     <!--Main Navigation-->
     <header>
 
@@ -152,6 +142,17 @@
     <!--Main layout-->
 
     <!-- Footer -->
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <!-- Google Fonts Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="<?= base_url(); ?>material/css/bootstrap.min.css">
+    <!-- Material Design Bootstrap -->
+    <link rel="stylesheet" href="<?= base_url(); ?>material/css/mdb.min.css">
+    <!-- Your custom styles (optional) -->
+    <link rel="stylesheet" href="<?= base_url(); ?>material/css/style.css">
 </body>
 <!-- jQuery -->
 <script type="text/javascript" src="<?= base_url(); ?>material/js/jquery.min.js"></script>
