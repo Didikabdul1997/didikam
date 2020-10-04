372853040f531e6b1562139f2e09f5f60cf2f43f
Lenovo <>
2020-10-04 13:53:37 +0700

    Update templates.php

--- application/views/depan/templates.php
+++ application/views/depan/templates.php
@@ -143,6 +143,16 @@
 
     <!-- Footer -->
 
+    <!-- Font Awesome -->
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css"> -->
+    <!-- Google Fonts Roboto -->
+    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"> -->
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="<?= base_url(); ?>material/css/bootstrap.min.css">
+    <!-- Material Design Bootstrap -->
+    <!-- <link rel="stylesheet" href="<?= base_url(); ?>material/css/mdb.min.css"> -->
+    <!-- Your custom styles (optional) -->
+    <!-- <link rel="stylesheet" href="<?= base_url(); ?>material/css/style.css"> -->
 </body>
 <!-- jQuery -->
 <script type="text/javascript" src="<?= base_url(); ?>material/js/jquery.min.js"></script>
